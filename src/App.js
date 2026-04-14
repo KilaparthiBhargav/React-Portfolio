22a4ac464e4f1f48c0dc73647a0b9c7c4e07c510
KilaparthiBhargav <bhargav908716@gmail.com>
2026-04-14 22:55:27 +0530

    fix build errors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 
 import './App.css';
-import About from './Componants/About';
-import Contact from './Componants/Contact';
-import Footer from './Componants/Footer';
-import { Header } from './Componants/Header';
-import Home from './Componants/Home';
-import Projects from './Componants/Projects';
-import Resume from './Componants/Resume';
 import HomePage from './Componants/homePage';
 
 function App() {
   return (
     <div className="App">
-    {/* <Header />
-    <Home />
-    <About />
-    <Projects />
-    <Resume />
-    <Contact />
-    <Footer /> */}
     <HomePage />
     </div>
   );
