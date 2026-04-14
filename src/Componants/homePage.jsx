611f4adf669515ca1fa374d16f9a4108fa8f5e62
KilaparthiBhargav <bhargav908716@gmail.com>
2026-04-14 23:01:35 +0530

    fix vercel build errors

--- src/Componants/homePage.jsx
+++ src/Componants/homePage.jsx
@@ -17,7 +17,7 @@ export default function Home() {
         <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
 
           {/* LOGO */}
-          <a href="#home" className="text-xl font-bold text-white">
+          <a href="/" className="text-xl font-bold text-white">
             Kilaparthi Bhargav
           </a>
 
